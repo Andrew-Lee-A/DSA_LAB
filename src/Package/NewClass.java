53d4b2f91094e56da9e3e0bfe78c21fe70330e02
Andrew-Lee-A <79880562+Andrew-Lee-A@users.noreply.github.com>
2021-05-18 23:22:12 +1200

    new class

---
+++ src/Package/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Package;
+
+/**
+ *
+ * @author Lee5
+ */
+public class NewClass {
+    
+}
