850d4e67f533c65010b203fd784bb44f454e0880
Andrew-Lee-A <79880562+Andrew-Lee-A@users.noreply.github.com>
2021-05-24 17:00:55 +1200

    Week7
    Complete

--- src/Week7/LinkedListBinaryTreeNode.java
+++ src/Week7/LinkedListBinaryTreeNode.java
@@ -6,6 +6,7 @@
 package Week7;
 
 import java.util.Enumeration;
+import java.util.NoSuchElementException;
 import javax.swing.tree.MutableTreeNode;
 import javax.swing.tree.TreeNode;
 
@@ -16,69 +17,181 @@ import javax.swing.tree.TreeNode;
  */
 public class LinkedListBinaryTreeNode<E> implements MutableTreeNode {
 
+    private E element;
+    private MutableTreeNode root;
+    private MutableTreeNode parent;
+    private MutableTreeNode leftChild;
+    private MutableTreeNode rightChild;
+    
+    public LinkedListBinaryTreeNode(){
+        this(null);
+    }
+    
+    public LinkedListBinaryTreeNode(E element){
+        this.element = element;
+        root = null;
+        parent = null;
+        leftChild = null;
+        rightChild = null;
+    }
+    
     @Override
-    public void insert(MutableTreeNode child, int index) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void insert(MutableTreeNode child, int index) throws IllegalArgumentException, IndexOutOfBoundsException{
+        if(child == null)
+            throw new IllegalArgumentException("Null child");
+        if(index != 0 && index != 1)
+            throw new IndexOutOfBoundsException();
+        
+        if(index == 0)
+        {
+            child.removeFromParent();
+            leftChild = child;
+            leftChild.setParent(this.parent);
+        }
+        else
+        {
+            child.removeFromParent();
+            rightChild = child;
+            rightChild.setParent(this.parent);
+        }
     }
 
     @Override
     public void remove(int index) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(index == 0)
+        {
+            //this.leftChild.removeFromParent();
+            this.leftChild.setParent(null);
+            this.leftChild = null;
+        }    
+        else if(index == 1)
+        {
+            //this.rightChild.removeFromParent();
+            this.rightChild.setParent(null);
+            this.rightChild = null;
+        }
+        else
+            throw new IndexOutOfBoundsException();
     }
 
     @Override
     public void remove(MutableTreeNode node) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         if(node == leftChild)
+            remove(0);
+        if(node == rightChild)
+            remove(1);
+        
     }
 
     @Override
     public void setUserObject(Object object) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.element = (E) object;
     }
 
     @Override
     public void removeFromParent() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (this.parent!=null)
+        {  
+            parent.remove(this);
+            this.parent = null;
+        }
     }
 
     @Override
     public void setParent(MutableTreeNode newParent) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.parent = newParent;
     }
 
     @Override
-    public TreeNode getChildAt(int childIndex) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public TreeNode getChildAt(int childIndex) throws IndexOutOfBoundsException{
+        if(childIndex == 0)
+        {
+            return this.leftChild;
+        }
+        else if(childIndex == 1)
+        {
+            return this.rightChild;
+        }
+        else
+            throw new IndexOutOfBoundsException();
     }
 
     @Override
     public int getChildCount() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int count = 0;
+        if(leftChild != null){
+            count++;
+        }
+        if (rightChild != null){
+            count++;
+        }
+        return count;
     }
 
     @Override
     public TreeNode getParent() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this.parent;
     }
 
     @Override
-    public int getIndex(TreeNode node) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public int getIndex(TreeNode node) throws NoSuchElementException{
+        if(node == this.leftChild){
+            return 0;
+        }else if (node == this.rightChild){
+            return 1;
+        }else{
+            throw new NoSuchElementException();
+        }
     }
 
     @Override
     public boolean getAllowsChildren() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return true;
     }
 
     @Override
     public boolean isLeaf() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return (leftChild == null && rightChild == null);
     }
 
     @Override
     public Enumeration children() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    @Override
+    public String toString()
+    {  
+        String output = "" + this.element;
+        if (!isLeaf())
+        {  
+            output += "[ ";
+            output += ((leftChild==null)?"_":leftChild) + ":"+((rightChild==null)?"_":rightChild);   
+            output += " ] ";
+        }
+        return output;
+    }
+    
+    public static void main(String[] args) {
+
+      MutableTreeNode root = new LinkedListBinaryTreeNode<String>("A");
+      MutableTreeNode nodeB = new LinkedListBinaryTreeNode<String>("B");
+      MutableTreeNode nodeC = new LinkedListBinaryTreeNode<String>("C");
+      MutableTreeNode nodeD = new LinkedListBinaryTreeNode<String>("D");
+      MutableTreeNode nodeE = new LinkedListBinaryTreeNode<String>("E");
+      MutableTreeNode nodeF = new LinkedListBinaryTreeNode<String>("F");
+      // build the tree
+      nodeB.insert(nodeD, 0);
+      nodeB.insert(nodeE, 1);
+      nodeC.insert(nodeF, 0);
+      root.insert(nodeB, 0);
+      root.insert(nodeC, 1);
+      System.out.println("Original Tree: " + root);
+      root.remove(nodeC);
+      nodeB.insert(nodeC, 1);
+      System.out.println("Modified Tree: " + root);
+        
+        
     }
     
 }
